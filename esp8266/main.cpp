2caf1f907249776e6bb2baf19e1eb62462024ae1
Sergio Marchena <ortonchico_sergio@hotmail.com>
2018-04-29 14:57:32 +0200

    Actualizado con put
    Actualizado con put

--- esp8266/main.cpp
+++ esp8266/main.cpp
@@ -4,6 +4,7 @@
 #include <ESP8266Wifi.h>
 #include <RestClient.h>
 
+
 const char* ssid = "Orange-12A1";
 const char* pass = "375772F7";
 
@@ -26,22 +27,32 @@ void setup() {
 	Serial.println(WiFi.localIP());
 }
 
+
 void loop() {
+  //get de la base de datos
   String response = "";
-	int statusCode = client.get("/api/sensores/1",&response);
+	int statusCode = client.get("/api/sensores/7",&response);
 	Serial.println(statusCode);
-	Serial.println(response);
+  Serial.println(response);
 
-	const int size_t_capacity =
-		JSON_OBJECT_SIZE(2) +
-		JSON_ARRAY_SIZE(5) + 120;
+  const int size_t_capacity = JSON_OBJECT_SIZE(5) + JSON_ARRAY_SIZE(2) + 60;
+  DynamicJsonBuffer jsonBuffer(size_t_capacity);
+  JsonObject& root = jsonBuffer.parseObject(response);
 
-	DynamicJsonBuffer jsonBuffer(size_t_capacity);
-	JsonObject& root =jsonBuffer.parseObject(response);
+   if(!root.success()){
+         Serial.println("Parse Failed...");
+       }
 
-	const char* id = root["id"];
 	double valor = root["valor"];
-  Serial.println(id);
   Serial.println(valor);
 
+//put de la base de datos
+  StaticJsonBuffer<300> JSONbuffer;
+  JsonObject& JSONencoder = JSONbuffer.createObject();
+  JSONencoder["id"] = 7;JSONencoder["fecha"] = 23;JSONencoder["nombre"] = "delos";JSONencoder["valor"] = 89.0;JSONencoder["localizacion_nombre"] = "Habitacion";
+  char JSONmessageBuffer[300];
+  JSONencoder.printTo(JSONmessageBuffer);
+  int statusPut = client.put("/api/sensores/",JSONmessageBuffer);
+  Serial.println(statusPut);
+ delay(50000);
 }
