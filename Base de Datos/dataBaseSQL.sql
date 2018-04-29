5dcce6df0761c207e362162bbb2cf487949c5508
Sergio Marchena <ortonchico_sergio@hotmail.com>
2018-04-29 14:54:44 +0200

    Actualizada BD
    Actualizada BD

--- Base de Datos/dataBaseSQL.sql
+++ Base de Datos/dataBaseSQL.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `dad` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `dad`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: dad
 -- ------------------------------------------------------
--- Server version	5.5.5-10.1.21-MariaDB
+-- Server version	5.5.5-10.1.30-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,8 +26,8 @@ DROP TABLE IF EXISTS `actuadores`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `actuadores` (
   `id` int(11) NOT NULL,
-  `fecha` bigint(20) DEFAULT NULL,
-  `sentido` tinyint(1) DEFAULT NULL,
+  `velocidad` double DEFAULT NULL,
+  `sentido` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -130,7 +132,7 @@ DROP TABLE IF EXISTS `persianas`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `persianas` (
   `id` int(11) NOT NULL,
-  `estado` tinyint(1) DEFAULT NULL,
+  `estado` bigint(20) DEFAULT NULL,
   `localizacion_nombre` varchar(45) DEFAULT NULL,
   `id_actuador` int(11) NOT NULL,
   PRIMARY KEY (`id_actuador`),
@@ -159,13 +161,13 @@ DROP TABLE IF EXISTS `sensores`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sensores` (
   `id` int(11) NOT NULL,
-  `fecha` bigint(20) DEFAULT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
+  `fecha` datetime NOT NULL,
   `valor` double DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
   `localizacion_nombre` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `localizaciones_nombre_idx` (`localizacion_nombre`),
-  CONSTRAINT `localizacion_nombre_sensores` FOREIGN KEY (`localizacion_nombre`) REFERENCES `localizaciones` (`nombre`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`id`,`fecha`),
+  KEY `nombre_localizacion_sensores_idx` (`localizacion_nombre`),
+  CONSTRAINT `nombre_localizacion_sensores` FOREIGN KEY (`localizacion_nombre`) REFERENCES `localizaciones` (`nombre`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -175,7 +177,7 @@ CREATE TABLE `sensores` (
 
 LOCK TABLES `sensores` WRITE;
 /*!40000 ALTER TABLE `sensores` DISABLE KEYS */;
-INSERT INTO `sensores` VALUES (1,22,'luz',100,'Cocina'),(2,22,'lluvia',40,'Cocina'),(3,22,'luz',110,'Habitacion'),(4,22,'lluvia',50,'Habitacion');
+INSERT INTO `sensores` VALUES (1,'2014-12-01 00:00:00',100,'penya','Cocina'),(7,'2014-12-01 00:00:00',89,'delos','Habitacion'),(7,'2018-04-29 14:11:44',89,'delos','Habitacion'),(7,'2018-04-29 14:11:49',89,'delos','Habitacion'),(7,'2018-04-29 14:11:54',89,'delos','Habitacion'),(7,'2018-04-29 14:12:00',89,'delos','Habitacion'),(7,'2018-04-29 14:12:05',89,'delos','Habitacion'),(7,'2018-04-29 14:12:10',89,'delos','Habitacion'),(7,'2018-04-29 14:12:16',89,'delos','Habitacion'),(7,'2018-04-29 14:12:21',89,'delos','Habitacion'),(7,'2018-04-29 14:12:27',89,'delos','Habitacion'),(7,'2018-04-29 14:14:14',89,'delos','Habitacion'),(7,'2018-04-29 14:18:05',89,'delos','Habitacion'),(7,'2018-04-29 14:18:14',89,'delos','Habitacion'),(7,'2018-04-29 14:19:18',89,'delos','Habitacion'),(7,'2018-04-29 14:19:29',89,'delos','Habitacion'),(7,'2018-04-29 14:20:19',89,'delos','Habitacion'),(7,'2018-04-29 14:21:10',89,'delos','Habitacion'),(7,'2018-04-29 14:22:00',89,'delos','Habitacion'),(7,'2018-04-29 14:27:40',89,'delos','Habitacion'),(7,'2018-04-29 14:27:54',89,'delos','Habitacion'),(7,'2018-04-29 14:28:44',89,'delos','Habitacion'),(7,'2018-04-29 14:29:35',89,'delos','Habitacion'),(7,'2018-04-29 14:48:15',89,'delos','Habitacion'),(7,'2018-04-29 14:48:37',89,'delos','Habitacion'),(7,'2018-04-29 14:49:27',89,'delos','Habitacion');
 /*!40000 ALTER TABLE `sensores` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -188,4 +190,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-03 14:10:00
+-- Dump completed on 2018-04-29 14:53:09
